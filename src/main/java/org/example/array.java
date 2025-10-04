039fc887ebd1cd738d6c6cebc703efd7f39f81f1
joão victor <joao.nogueira@dcx.ufpb.br>
2025-10-04 14:37:33 -0300

    Estudos final

---
+++ src/main/java/org/example/array.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class array {
+    public static void main(String[]args){
+        Double[] notas= {9.8,9.3,10.0,7.1};
+        for(int i=0;i<notas.length;i++){
+            System.out.println("As notas foram excelentes, foram elas:"+notas[i]);
+        }
+    }
+}
